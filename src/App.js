54c637b01288da61b234f6d920c8fd90ae059777
leamaddy <leamaddy75@gmail.com>
2025-01-07 18:32:16 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import React from "react";
+// import logo from './logo.svg';
+
+import ScrollingDiv from "./Components";
+
+function App() {
+  return (
+    
+    <div>
+      <ScrollingDiv />
+    </div>
+  );
+}
+
+export default App;
